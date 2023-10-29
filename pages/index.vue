ff0262b4013eb2f43c6424a0a7d4b4baa1c57008
Alois Sečkár <ellrohir@seznam.cz>
2023-10-29 18:33:38 +0100

    feat: project description

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,47 @@
 <template>
+  <div>
     <div>
-        POPIS PROJEKTU + ROZCESTNÍK
+      V rámci projektu <a href="https://www.denproskolu.cz/">Den pro školu</a>
+      nabízím IT workshopy na téma <strong>"Vývoj webových aplikací pomocí frameworku Nuxt"</strong>.
+      Jsou určeny spíše pro pokročilejší studenty informatiky.
+      Pro žáky, kteří s programováním teprve začínají, je vhodnější workshop
+      <a href="https://web-4-school.netlify.app/">"Základy tvorby webových stránek"</a>.
     </div>
+    <h2>POPIS WORKSHOPU</h2>
+    <div>
+      Studenti se seznámí s frameworkem Nuxt určeným pro tvorbu webových aplikací v jazyce JavaScript.
+      Výklad bude probíhat názorně nad zdrojovým kódem tohoto projektu, který je veřejně k dispozici na
+      https://github.com/AloisSeckar/nuxt-4-school. Součástí bude též seznámení se a výklad (bude-li třeba)
+      jak funguje verzovácí nástroj Git a platforma GitHub a jak umožňují souběžnou práci více programátorů.
+      Cílem workshopu bude vytvoření osobní stránky každého účastníka, která bude přidána do tohoto projektu
+      a stane se jeho veřejnou součástí. Součástí procesu je tedy nejen samotné vývoj pomocí Nuxt,
+      ale i vyzkoušení si, jakým způsobem lze přispět do jiného programátorského projektu.
+      Účastníci budou mít do projektu možnost vstupovat i v budoucnu a své příspěvky dále vylepšovat.
+    </div>
+    <h2>PŘEDPOKLADY WORKSHOPU</h2>
+    <div>
+      Základní znalost HTML, CSS a JavaScriptu, nebo alespoň základní orientace v problematice programování
+      a schopnost zorientovat se v nové technologii na základě praktického výkladu přímo během workshopu.
+    </div>
+    <div>
+      Je třeba mít nainstalováno nebo mít možnost v rámci projektu nainstalovat následující aplikace:
+    </div>
+    <ul>
+      <li>
+        <strong>Git</strong> - verzovací software pro kolaborativný vývoj aplikací
+        <strong>GitHub desktop</strong> - integrace pro snadné napojení na projekt
+        <strong>VS Code</strong> - vývojové prostředí pro programování
+        <strong>Node.js</strong> - lokální běhové prostředí pro JavaScriptové aplikace
+      </li>
+    </ul>
+    <h2>ROZCESTNÍK PROJEKTŮ</h2>
+    <div>
+      Zde naleznete odkazy na jednotlivé proběhlé workshopy a osobní stránky jednotlivých studentů vytvořené během projektů.
+    </div>
+    <ul>
+      <li>
+        <strong>20.11.2023</strong> - AHOL - Vyšší odborná škola, Ostrava
+      </li>
+    </ul>
+  </div>
 </template>
