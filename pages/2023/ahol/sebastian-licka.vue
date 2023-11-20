90076d7ff800ab37e012bfb3965bfd3b28b98e18
Xerepus <sebatian.licka@hotmail.cz>
2023-11-20 14:15:05 +0100

    ahol workshop 20.11.2023

---
+++ pages/2023/ahol/sebastian-licka.vue
@@ -0,0 +1,11 @@
+<template>
+<div class="m-2 big teal-800">
+    <PersonalCard  first-name="Sebastian" last-name="Lička"
+    image="/fotka.jpg"
+    :age=23
+    place="Česko"
+    about="Existuju"
+    background="#fef9c3"
+    />
+</div>
+</template>
