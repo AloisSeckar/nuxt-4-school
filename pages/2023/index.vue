800329330e0762aa61b4dd7cc5d93c04bb72229e
Alois Sečkár <ellrohir@seznam.cz>
2023-11-20 16:25:44 +0100

    feat: 2023 index page

--- pages/2023/index.vue
+++ pages/2023/index.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
-    2023
+    <h2>2023</h2>
+    <p>
+      V roce 2023 proběhla akce <strong>20.&nbsp;listopadu</strong> ve
+      <a href="https://vos.ahol.cz/">Vyšší odborné škole AHOL v&nbsp;Ostravě</a>.
+      Zúčastnilo se <strong>5</strong> studentů a&nbsp;prezentaci jejich výstpů
+      naleznete <a href="/2023/ahol">ZDE</a>.
+    </p>
   </div>
 </template>
